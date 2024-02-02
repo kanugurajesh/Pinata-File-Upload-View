95126b8a91cd8f3b10ca2e224f01000a21131255
kanugurajesh <kanugurajesh3@gmail.com>
2024-02-02 09:11:19 +0530

    Adding file uploading feature to the ap

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,45 @@
+"use client";
+
+import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
+
 export default function Home() {
-    return (
-      <main>
-        
-      </main>
-    );
-  }
-  
+  const [file, setFile] = useState<File>();
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!file) return;
+
+    try {
+      const data = new FormData();
+      data.set("file", file);
+
+      const res = await fetch("/api/upload", {
+        method: "POST",
+        body: data,
+      });
+
+      if (!res.ok) {
+        toast.error("Failed to upload file");
+        throw new Error(await res.text());
+      }
+    } catch (e: any) {
+      // toast.error(e);
+      console.log(e);
+    }
+  };
+
+  return (
+    <main>
+      <Toaster />
+      <form onSubmit={onSubmit}>
+        <input
+          type="file"
+          name="file"
+          onChange={(e) => setFile(e.target.files?.[0])}
+        />
+        <input type="submit" value="upload" />
+      </form>
+    </main>
+  );
+}
---
+++ app/api/upload/route.ts
@@ -0,0 +1,25 @@
+import { writeFile } from "fs";
+import { NextRequest, NextResponse } from "next/server";
+import { join } from "path";
+
+export async function POST(request: NextRequest) {
+  const data = await request.formData();
+  const file: File | null = data.get("file") as unknown as File;
+
+  if (!file) {
+    return NextResponse.json({ success: false });
+  }
+
+  const bytes = await file.arrayBuffer();
+  const buffer = Buffer.from(bytes);
+
+  const path = join("/", "tmp", file.name);
+  await writeFile(path, buffer, (err) => {
+    if (err) {
+      console.error(err);
+      return NextResponse.json({ success: false });
+    }
+  });
+
+  return NextResponse.json({ success: true });
+}
